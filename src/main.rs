4bd587d59263e957fe34bbf22334bf7c74fa08c5
Jerell James <jerell1998@gmail.com>
2022-11-06 17:07:12 +0000

    removed forward pass example

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,6 @@ pub mod value;
 
 fn main() {
     {
-        //forward pass example
-        let a = Value::new(2.0, "a");
-        let b = Value::new(-3.0, "b");
-        let c = Value::new(10.0, "c");
-        let mut e = &a * &b;
-        e.label("e");
-        let mut d = &e + &c;
-        d.label("d");
-        let f = Value::new(-2.0, "f");
-
-        let mut l = &d * &f;
-        l.label("L");
-
-        l.grad(1.0);
-
-        println!("{l}");
-    }
-
-    {
         // backprop example
         let x1 = Value::new(2.0, "x1");
         let x2 = Value::new(0.0, "x2");
